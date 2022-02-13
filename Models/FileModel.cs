a57b29196a81cf7dc4bf0ee406aa131c034fe8d5
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-02-13 15:56:37 +0700

    Delete Models directory

--- Models/FileModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class FileModel
-    {
-        public string FileName { get; set; }
-        public string FilePath { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
